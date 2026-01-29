21e7c3affcfaf9c34028332633d46658c18f1aec
McSwaggens <McSwaggens@protonmail.com>
2026-01-29 19:43:01 +1100

    Clean up HLSL shader formatting and remove column_major qualifier

--- frag.hlsl
+++ frag.hlsl
@@ -1,15 +1,12 @@
-struct PSInput
-{
+struct PSInput {
     [[vk::location(0)]] float3 vertex_color : COLOR;
 };
 
-struct PSOutput
-{
+struct PSOutput {
     [[vk::location(0)]] float4 color : SV_Target0;
 };
 
-PSOutput main(PSInput input)
-{
+PSOutput main(PSInput input) {
     PSOutput output;
     output.color = float4(input.vertex_color, 1.0f);
     return output;
--- vert.hlsl
+++ vert.hlsl
@@ -1,27 +1,24 @@
 [[vk::binding(0, 0)]]
-cbuffer Ubo : register(b0)
-{
+cbuffer Ubo : register(b0) {
     float time;
-    column_major float4x4 vp;
+    float4x4 vp;
 };
 
-struct VSInput
-{
+struct VSInput {
     [[vk::location(0)]] float3 in_pos   : POSITION;
     [[vk::location(1)]] float3 in_color : COLOR;
     [[vk::location(2)]] float3 position : TEXCOORD0;
 };
 
-struct VSOutput
-{
+struct VSOutput {
     float4 pos : SV_Position;
     [[vk::location(0)]] float3 frag_color : COLOR;
 };
 
-VSOutput main(VSInput input)
-{
+VSOutput main(VSInput input) {
     VSOutput output;
     output.pos = mul(vp, float4(input.in_pos + input.position, 1.0f));
     output.frag_color = input.in_color;
     return output;
 }
+
